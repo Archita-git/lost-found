8e9b0724aa63fbf4978c972e19406b4902615e8d
Archita R Pai <archupai05@gmail>
2025-12-05 18:30:46 +0530

    added requirements.txt

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,12 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL,
-    pool_size=5,
-    max_overflow=0,
-    pool_timeout=30,
-    pool_recycle=1800,
-    )
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
